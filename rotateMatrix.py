e5d81fb2ff520d06b6acd4ce00ec8bc7d183fd97
MrErikasMr <129548614+MrErikasMr@users.noreply.github.com>
2024-01-09 00:16:34 +0000

    Add files via upload

--- rotateMatrix.py
+++ rotateMatrix.py
@@ -1,17 +1,129 @@
 #rotate a matrix by 90 degrees
 #this is a game, which involves going right, down, left, up.
 
+#successfully rotated the outside of the matrix, now we have to delve deeper and rotate the insides
 class RotateMatrix():
     def __init__(self):
         self.matrix = [
             [0,1,2],
-            [3,4,5],
-            [6,7,8]
+            [4,5,6],
+            [8,9,10]
+
+
         ]
-        self.matrix2 = self.matrix
+        self.matrix2 = [
+            [0,0,0],
+            [0,0,0],
+            [0,0,0],
+
+
+        ]
 
     def matrix_rotator(self):
         return self.matrix
+
+    def rotate_matrix(self):
+        matrix_length = len(instance.matrix[0])
+        matrix_width = len(instance.matrix)
+        matrix_dimension = matrix_length * matrix_width
+        #  print(len(instance.matrix))
+        x_coordinate = 0
+        y_coordinate = 0
+        x_rotation = 0
+        y_rotation = 0
+        x_min = 0
+        y_min = 0
+        x_max = matrix_length - 1
+
+        y_max = matrix_width - 1
+        current_coordinate = [0, 0]
+        going_right = True
+        going_down = False
+        going_left = False
+        going_up = False
+        print("X max:", x_max)
+
+        for x in range(matrix_dimension + 15):
+            try:
+
+                if x == 0:
+                    number = int(instance.matrix[y_coordinate][x_coordinate])
+                    y_rotation = 0
+                    x_rotation = x_max
+                    instance.matrix2[y_rotation][x_rotation] = number
+                    x_coordinate += 1
+                    y_coordinate = 0
+                    y_rotation += 1
+                    continue
+
+                if going_right:
+                    if x_coordinate > x_max:
+                        going_right = False
+                        going_down = True
+                        x_coordinate = x_max
+                        y_coordinate += 1
+                        y_rotation -= 1
+                        x_rotation -= 1
+
+                        continue
+
+                    number = int(instance.matrix[y_coordinate][x_coordinate])
+                    instance.matrix2[y_rotation][x_rotation] = number
+                    x_coordinate += 1
+                    y_rotation += 1
+                    print(x_coordinate)
+
+                if going_down:
+                    if y_coordinate > y_max:
+                        going_down = False
+                        going_left = True
+                        y_coordinate = y_max
+                        x_coordinate -= 1
+                        x_rotation = 0
+                        y_rotation -= 1
+                        continue
+
+                    #    print(y_coordinate, x_coordinate)
+                    number = int(instance.matrix[y_coordinate][x_coordinate])
+                    instance.matrix2[y_rotation][x_rotation] = number
+                    x_rotation -= 1
+                    y_coordinate += 1
+
+                if going_left:
+                    if x_coordinate < x_min:
+                        going_left = False
+                        going_up = True
+                        x_coordinate = x_min
+                        y_coordinate -= 1
+
+                        y_rotation = 0
+                        x_rotation += 1
+                        continue
+
+                    number = int(instance.matrix[y_coordinate][x_coordinate])
+                    instance.matrix2[y_rotation][x_rotation] = number
+                    y_rotation -= 1
+                    x_coordinate -= 1
+
+                if going_up:
+                    if y_coordinate < y_min:
+
+                        return instance.matrix2
+                    number = int(instance.matrix[y_coordinate][x_coordinate])
+                    instance.matrix2[y_rotation][x_rotation] = number
+                    x_rotation += 1
+                    y_coordinate -= 1
+
+
+
+
+
+
+
+
+            except IndexError:
+                print("indexError")
+                continue
 
 
 instance = RotateMatrix()
@@ -24,30 +136,21 @@ for array in instance.matrix:
 
 
 
-def rotate_matrix():
-    matrix_length = len(instance.matrix[0])
-    x_coordinate = 0
-    y_coordinate = 0
-    x_min = 0
-    x_max = matrix_length - 1
-    #y_max =
-    current_coordinate = [0,0]
-    for x in range(len(instance.matrix)):
-        if x == 0:
-            y_coordinate = instance.matrix2[0][matrix_length - 1]
-
-            instance.matrix2[x_coordinate][y_coordinate] = instance.matrix[x_coordinate][x]
-            current_coordinate = [0,2]
-            continue
-        if(current_coordinate[0] == x_min):
-            pass
 
 
 
 
 
 
-rotate_matrix()
+
+
+
+instance.rotate_matrix()
 print("Matrix 2: ")
 for array in instance.matrix2:
+    print(array)
+
+print("")
+print("Matrix 1: ")
+for array in instance.matrix:
     print(array)
--- test_rotateMatrix.py
+++ test_rotateMatrix.py
@@ -14,3 +14,26 @@ def test_matrix_is_matrix():
     for item in instance.matrix_rotator():
         assert type(item) is list
 
+
+
+
+def test_3_by_3():
+    result = [[8, 4, 0],
+        [9, 0, 1],
+        [10, 6, 2]]
+
+    assert result == instance.rotate_matrix()
+
+
+def test_4_by_4():
+    pass
+    result = [[12, 8, 4, 0],
+        [13, 0, 0, 1],
+        [14, 0, 0, 2],
+        [15, 11, 7, 3]]
+
+    assert result == instance.rotate_matrix()
+
+
+
+
