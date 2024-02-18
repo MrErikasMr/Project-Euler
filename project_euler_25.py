235cc71ec043576fba3e48015331c3493e9d235f
MrErikasMr <129548614+MrErikasMr@users.noreply.github.com>
2024-02-18 23:54:17 +0000

    Add files via upload

--- project_euler_25.py
+++ project_euler_25.py
@@ -1,26 +1,24 @@
-##non abundant sums
+array = []
+abundant_number = []
 
-
-big_array = []
-for x in range(1, 100):
-    #print(x,"x")
-    factor_array = []
-    for i in range(1,x):
-        if x % i == 0:
-            factor_array.append(i)
-            #print(i)
-
-    if sum(factor_array) > x:
-        print(x, "YO")
-        big_array.append(x)
-
-print(big_array)
-
-
-for y in range(1,big_array[-1]):
-    for z in range(1,big_array[-1]):
-        # find all the numbers that are smaller than let's say "40" and then create an array of all those numbers
-    # and then try to add those numbers together 1 by 1, and if we get a sum that is bigger, move onto the next integer
+for x in range(1,2000):
+    for y in range(1,x):
+        #print(y,"y")
+        if x % y == 0:
+         #   print(y,"divisible")
+            array.append(y)
+   # print(x,"x")
+  #  print(array)
+    if (sum(array) > x):
+        abundant_number.append(x)
+    array = []
 
 
 
+print(abundant_number)
+
+
+for a in range(len(abundant_number)):
+    print(abundant_number[a])
+    for z in range(abundant_number[a]):
+        print(abundant_number[z])
