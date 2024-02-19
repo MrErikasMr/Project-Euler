a5cdab93259c3d22efb9bab3002b3c2e97f0c65b
MrErikasMr <129548614+MrErikasMr@users.noreply.github.com>
2024-02-19 00:35:09 +0000

    Add files via upload

--- project_euler_25.py
+++ project_euler_25.py
@@ -1,7 +1,7 @@
 array = []
 abundant_number = []
 
-for x in range(1,2000):
+for x in range(1,28124):
     for y in range(1,x):
         #print(y,"y")
         if x % y == 0:
@@ -15,10 +15,27 @@ for x in range(1,2000):
 
 
 
-print(abundant_number)
+#print(abundant_number)
 
-
+bigger_array = []
 for a in range(len(abundant_number)):
-    print(abundant_number[a])
-    for z in range(abundant_number[a]):
-        print(abundant_number[z])
+    #print(abundant_number[a])
+    number = abundant_number[a]
+    to_sum_array = abundant_number[abundant_number.index(abundant_number[a]):]
+    #print(to_sum_array)
+    for z in range(len(to_sum_array)):
+      #  print(number + to_sum_array[z])
+        bigger_array.append(number + to_sum_array[z])
+   #     print(bigger_array)
+
+
+print(bigger_array)
+biggest_array = []
+
+for i in range(1,28124):
+    if i not in bigger_array:
+        biggest_array.append(i)
+
+print(biggest_array)
+print(sum(biggest_array))
+
