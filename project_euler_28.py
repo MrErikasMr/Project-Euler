10a977380da51555aa2fee5bc799a68f290d2dd9
MrErikasMr <129548614+MrErikasMr@users.noreply.github.com>
2024-02-20 12:31:05 +0000

    Add files via upload

--- project_euler_28.py
+++ project_euler_28.py
@@ -1,4 +1,18 @@
+
+longest_string = ""
+number = 0
 for x in range(1,1000):
     #print(1/x)
     num = 1/x
-    print(f"{num:.1000f}")
+   # print(f"{num:.100f}")
+    string_num = str(num)
+    #print(string_num)
+    if len(string_num) > 6:
+        print(string_num)
+        print(string_num[3:15])
+
+
+
+
+
+
