c494b10a66778b2b1269be61a43dde9d7744d8e3
MrErikasMr <129548614+MrErikasMr@users.noreply.github.com>
2024-02-22 16:00:05 +0000

    Add files via upload

--- project_euler_28.py
+++ project_euler_28.py
@@ -1,27 +1,53 @@
+#Find the unit fraction below 1000 with the longest reciprocal
+#example: 1/3 = 0.333 (only 1 recurring digit)
+# 1/7 = 0.142857142857 (6 digit reucurring)
 
 
-text = "hello"
+print(1/7)
 
+recurring_array = []
+y_array = []
 
-
-length_of_text = len(text)
-
-can_upper = True
-
-
-
-
-
-
-
-
-
-for x in range(1, 1000):
-
-    print(1/x)
+y = 5
+while y < 1001:
+    string = str(1 / y)
+    if len(string) < 6:
+        y += 1
+        continue
 
 
 
+    for x in range(2,len(string)):
+        print(y)
+
+
+        instance1 = int((x/2) + 1)
+
+        recurring1 = string[2:instance1]
+        value = int(x/2)
+
+        recurring2 = string[value + 1: value * 2]
+
+        if recurring1 == recurring2 and len(recurring1) > 5:
+            recurring_array.append(recurring1)
+            y_array.append(y)
+            y += 1
+            break
+        if x == len(string) - 1:
+
+            y += 1
+            break
 
 
 
+print(y_array)
+print(recurring_array)
+print(max(recurring_array))
+
+longest = ""
+
+for z in range(len(recurring_array)):
+    if len(recurring_array[z]) > len(longest):
+        longest = recurring_array[z]
+
+print(longest)
