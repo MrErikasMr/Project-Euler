fff1aa80f5c8a836ce5b447a3dd3906406ccb508
MrErikasMr <129548614+MrErikasMr@users.noreply.github.com>
2024-02-28 16:19:13 +0000

    Add files via upload

--- project_euler_29.py
+++ project_euler_29.py
@@ -8,7 +8,7 @@ $n^2 + an + b$, where $|a| &lt; 1000$ and $|b| \le 1000$<br><br><div>where $|n|$
 </blockquote>
 <p>Find the product of the coefficients, $a$ and $b$, for the quadratic expression that produces the maximum number of primes for consecutive values of $n$, starting with $n = 0$.</p>"""
 
-
+import math
 
 def primes(n,a,b,c):
     print(n)
@@ -20,4 +20,72 @@ def primes(n,a,b,c):
 
 
 
-primes(1,1,1,1)
+#primes(1,1,1,1)
+
+
+
+##sieve:
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def print_primes(n,count):
+    count = 0
+    num = 2
+    while count < n:
+        if is_prime(num):
+            print(num, end=' ')
+            count += 1
+        num += 1
+
+
+a = -1000
+b = -1000
+results_array = []
+results_tuple = ()
+
+string_formula = ""
+while True:
+    if b >= 1001:
+        b = 1
+        a += 1
+
+
+
+    if a >= 1001:
+        break
+    for x in range(1, 1000):
+        result = (x ** 2) + (a * x) + b
+
+        if is_prime(result) == False:
+            if len(results_array) > len(results_tuple):
+                results_tuple = tuple(results_array)
+                string_formula = ("%s^2 + %s*x + %s"%(x,a,b))
+                print(string_formula)
+
+            b += 1
+
+
+
+            results_array = []
+            break
+        results_array.append(result)
+        #print(x, result)
+
+
+print(a)
+
+print(results_tuple)
+print(string_formula)
+print(len(results_tuple))
