1534c4a8f8295c7af79708eacc9f6f788bf5b879
MrErikasMr <129548614+MrErikasMr@users.noreply.github.com>
2024-03-02 17:43:31 +0000

    Add files via upload

---
+++ project_euler_32.py
@@ -0,0 +1,9 @@
+#produce a partition function, and then apply the logic to make up all possibilities to get to £2.00 using pound sterling coins
+
+x = 1
+
+while True:
+
+    if x >= 5:
+        break
+
