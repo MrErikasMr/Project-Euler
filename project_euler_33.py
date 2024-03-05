36b2a053b448721e63afa4bff502adc32a11c3dc
MrErikasMr <129548614+MrErikasMr@users.noreply.github.com>
2024-03-05 15:59:55 +0000

    Add files via upload

---
+++ project_euler_33.py
@@ -0,0 +1,48 @@
+
+total_sum = 0
+big_array = []
+for x in range(1,1000):
+    for y in range(1,x):
+        string_x = str(x)
+        string_y = str(y)
+        product = x * y
+        string_product = str(product)
+
+        sum_of_lengths = len(string_x) + len(string_y) + len(string_product)
+
+        if (sum_of_lengths == 9) and (product not in big_array):
+
+            big_array.append(product)
+            actual_string = string_x + string_y + string_product
+            if '0' not in actual_string:
+
+  #          print(actual_string)
+
+ #           print(string_x + " x " + string_y + " = " + string_product)
+
+
+#            print(sorted(actual_string))
+
+                string = ""
+                actual_string = sorted(actual_string)
+
+
+                for z in range(len(actual_string)):
+                    if actual_string[z] not in string:
+                        string += actual_string[z]
+
+                if len(string) == 9:
+                 #   print(string)
+                    print(sorted(string))
+                    print(actual_string)
+                    total_sum += product
+                    print (string_x + " x " + string_y + " = " + string_product)
+                    print(product)
+
+
+
+
+
+print(len(big_array))
+print(total_sum)
+
