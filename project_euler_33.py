b761857ee81253efb1850c10dde8c00f2011c5da
MrErikasMr <129548614+MrErikasMr@users.noreply.github.com>
2024-03-06 06:56:33 +0000

    Add files via upload

--- project_euler_33.py
+++ project_euler_33.py
@@ -1,5 +1,6 @@
 
 total_sum = 0
+total_sum_array = []
 big_array = []
 for x in range(1,1000):
     for y in range(1,x):
@@ -9,35 +10,49 @@ for x in range(1,1000):
         string_product = str(product)
 
         sum_of_lengths = len(string_x) + len(string_y) + len(string_product)
+        actual_string = string_x + string_y + string_product
 
-        if (sum_of_lengths == 9) and (product not in big_array):
+        if (sum_of_lengths == 9) and '0' not in actual_string:
+
+           # print(string_x + " x " + string_y + " = " + string_product)
+
+            #  print(actual_string)
 
             big_array.append(product)
-            actual_string = string_x + string_y + string_product
-            if '0' not in actual_string:
+
 
   #          print(actual_string)
 
- #           print(string_x + " x " + string_y + " = " + string_product)
+ #           prin   t(string_x + " x " + string_y + " = " + string_product)
 
 
 #            print(sorted(actual_string))
 
+            string = ""
+            actual_string = sorted(actual_string)
+
+
+            for z in range(len(actual_string)):
+              #  print(actual_string[z])
+                if actual_string[z] not in string:
+                    string += actual_string[z]
+                else:
+                    break
+              #  print(string)
+
+            if len(string) >= 9:
+
+             #   print(string)
+              #  print(sorted(string))
+                print(actual_string)
+                total_sum += product
+                print (string_x + " x " + string_y + " = " + string_product)
+                print(product)
                 string = ""
-                actual_string = sorted(actual_string)
-
-
-                for z in range(len(actual_string)):
-                    if actual_string[z] not in string:
-                        string += actual_string[z]
-
-                if len(string) == 9:
-                 #   print(string)
-                    print(sorted(string))
-                    print(actual_string)
-                    total_sum += product
-                    print (string_x + " x " + string_y + " = " + string_product)
-                    print(product)
+            else:
+                continue
+            if product not in total_sum_array:
+                total_sum_array.append(product)
 
 
 
@@ -45,4 +60,5 @@ for x in range(1,1000):
 
 print(len(big_array))
 print(total_sum)
-
+print(sum(total_sum_array))
+print(total_sum_array)
