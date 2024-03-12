0efd9c3642e2535f618119bab79d4e8c9cb98013
MrErikasMr <129548614+MrErikasMr@users.noreply.github.com>
2024-03-12 15:02:08 +0000

    Add files via upload

--- project_euler_42.py
+++ project_euler_42.py
@@ -1,31 +1,54 @@
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+
+
+
+starting_string = '98'
+
+target_length = 9
+
+finishing_string = '7654321'
+
+index = 0
+start = 1
 big_array = []
-small_array = []
 
-def sieve_of_eratosthenes(n):
-    primes = [True] * ( n +1)
-    primes[0] = primes[1] = False
-
-
-    for i in range(2, int(n**0.5 ) +1):
-        if primes[i]:
-            string_i = str(i)
-
-                #small_array = []
-
-
-            for j in range( i *i, n+ 1, i):
-                primes[j] = False
-
-    return [i for i in range(2, n + 1) if primes[i]]
+list1 = ['7','6','5','4','3','2','1']
 
 
 
+def swap_positions(list, pos1, pos2):
+
+    list[pos1], list[pos2] = list[pos2],list[pos1]
+
+    return ''.join(list)
 
 
 
-prime_array = sieve_of_eratosthenes(10000)
+big_array.append(987654321)
+
+first_number = -2
+second_number = -1
+while True:
+    big_array.append(int('98' + swap_positions(list1,first_number,second_number)))
+    second_number -= 1
+    print(big_array)
+
+print(big_array)
 
 
-print(prime_array)
+# while True:
+#     new_string = finishing_string[start:] + finishing_string[index]
+#     print(new_string)
+#     big_array.append(new_string)
+#     new_string = ""
+#
+#     index += 1
 
 
