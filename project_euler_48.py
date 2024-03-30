5a078f094f81a1119d80050e22adaceb6149d527
MrErikasMr <129548614+MrErikasMr@users.noreply.github.com>
2024-03-30 17:16:20 +0000

    Add files via upload

--- project_euler_48.py
+++ project_euler_48.py
@@ -46,13 +46,19 @@ while True:
         if is_prime(total_sum):
             print('yes')
             dict1[total_sum] = prime_array[:y + 1]
-            if total_sum > 990 and total_sum < 1000:
+            if total_sum > 1000:
                 can_break = True
                 break
 
 
 
 print(dict1)
-print(total_sum)
 print(prime_array)
 print("yo")
+
+
+array1 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157]
+array2 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157]
+print(len(array2))
+if array1 == array2:
+    print('yes')
